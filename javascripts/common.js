280792e4e8f5b347655ecc6fbe0379e383128877
zydecx <fufengchu@gmail.com>
2015-10-06 17:56:01 +0800

    add adaptive response to themes

--- javascripts/common.js
+++ javascripts/common.js
@@ -1,5 +1,4 @@
 (function() {
-	console.log("fdsf");
 	$(".menu-popup").on("mouseover", "li", function(e) {
 		$(this).children(".sub-menu").addClass("active");
 	});
@@ -8,5 +7,16 @@
 		$(".sub-menu.active", this).removeClass("active");
 	});
 
+	$(".menu-slide").on("click", "li .menu", function(e) {
+		$(this).closest("li").children(".sub-menu").toggleClass("active");
+		$(this).toggleClass("active");
+	});
+
+	$(".main-menu li").each(function() {
+		var $_childUl = $(this).children(".sub-menu");
+		if ($_childUl.length > 0 && $_childUl.children("li").length > 0) {
+			$(this).children(".menu-wrapper").find(".menu.hide").removeClass("hide");
+		}
+	});
 
 })();
